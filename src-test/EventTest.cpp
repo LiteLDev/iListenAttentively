1f68c9ce607e82a2f30657b31379f7d10f4bb133
zimuya4153 <limulu10@163.com>
2025-03-02 00:13:58 +0800

    refactor: refactor event test

--- src-test/EventTest.cpp
+++ src-test/EventTest.cpp
@@ -1,34 +1,35 @@
 #include "ila/base/Gloabl.h"
-#include "ila/include_all.h"
-#include <ll/api/event/server/ServerStartedEvent.h>
-#include <ll/api/service/GamingStatus.h>
 
-struct EventTest
-{
-protected:
-    std::vector<ll::event::ListenerPtr> mListeners;
-
-public:
+inline struct EventTest {
     EventTest();
-    ~EventTest()
-    {
-        for (auto& listener : mListeners) { LLEventBus.removeListener(listener); }
-    }
-};
+    ~EventTest() = default;
+} test;
 
-inline struct StartTest
+EventTest::EventTest()
 {
-    StartTest()
+    static std::vector<ll::event::EventIdView> mEventList;
+    static auto const&                         callback =
+        ll::event::Listener<ll::event::Event>::create([](ll::event::Event& event) -> void {
+            auto const& it = std::find(mEventList.begin(), mEventList.end(), event.getId());
+            if (it != mEventList.end())
+            {
+                CompoundTag nbt;
+                event.serialize(nbt);
+                SelfLogger.warn(
+                    "Event {0} triggered, remaining {1} events to be triggered, data: {2}",
+                    event.getId().name,
+                    mEventList.size(),
+                    nbt.toSnbt()
+                );
+                mEventList.erase(it);
+            }
+        });
+    for (auto const& eventName : LLEventBus.events(ll::mod::NativeMod::current()->getName()))
     {
-        if (ll::getGamingStatus() != ll::GamingStatus::Running)
+        mEventList.emplace_back(eventName);
+        if (!LLEventBus.addListener(callback, eventName))
         {
-            LLEventBus.emplaceListener<ll::event::ServerStartedEvent>(
-                [](ll::event::ServerStartedEvent&) -> void { static EventTest test; }
-            );
+            SelfLogger.error("Failed to add listener for event: {0}", eventName.name);
         }
-        else { static EventTest test; }
     }
-} StartTest;
-
-
-EventTest::EventTest() {}
+}
