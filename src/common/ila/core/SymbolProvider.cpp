6c38d938b0a503760a1be3e715bffc6dee1feba2
子沐呀 <163636894+zimuya4153@users.noreply.github.com>
2026-02-25 22:09:56 +0800

    fix: fix encrypt imports

--- src/common/ila/core/SymbolProvider.cpp
+++ src/common/ila/core/SymbolProvider.cpp
@@ -195,12 +195,12 @@ ExternC FARPROC WINAPI __delayLoadHelper2(PCImgDelayDescr pidd, FARPROC* ppfnIAT
     if (!pImgDelayDescr_BDS && !strcmp(dli.szDll, BDSAPI_FAKEDLL_NAME)) { pImgDelayDescr_BDS = pidd; }
 
     if (pImgDelayDescr_BDS == pidd) {
-        auto symbol = new char[__strlen(dli.dlp.szProcName) + 1];
-        __memcpy(symbol, dli.dlp.szProcName, __strlen(dli.dlp.szProcName) + 1);
+        auto length = __strlen(dli.dlp.szProcName) + 1;
+        auto symbol = new char[length];
+        __memcpy(symbol, dli.dlp.szProcName, length);
         constexpr uint8_t mXorKey = 0;
-        for (size_t i = 0; i < __strlen(dli.dlp.szProcName); ++i) {
-            symbol[i] ^= mXorKey;
-        }
+        for (size_t i = 0; i < length; ++i) symbol[i] ^= mXorKey;
+        symbol[length - 1] = '\0';
         pfnRet = (FARPROC)resolveSymbol(symbol);
         delete[] symbol;
         goto SetEntryHookBypass;
