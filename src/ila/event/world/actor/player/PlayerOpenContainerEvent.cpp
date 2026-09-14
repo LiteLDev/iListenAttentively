9b236206c75c6b090393996e97eb3a0b247fbece
ShrBox <shrbox@outlook.com>
2026-09-15 00:16:57 +0800

    refactor fix and refactor PlayerOpenContainerEvent

--- src/ila/event/world/actor/player/PlayerOpenContainerEvent.cpp
+++ src/ila/event/world/actor/player/PlayerOpenContainerEvent.cpp
@@ -1,15 +1,18 @@
 #include "ila/event/world/actor/player/PlayerOpenContainerEvent.h"
 #include "ila/base/Gloabl.h"
-#include "ila/event/server/SendPacketEvent.h"
+#include <mc/deps/ecs/WeakEntityRef.h>
+#include <mc/deps/shared_types/legacy/ContainerType.h>
 #include <mc/legacy/ActorUniqueID.h>
 #include <mc/network/packet/ContainerOpenPacket.h>
+#include <mc/server/module/VanillaServerGameplayEventListener.h>
+#include <mc/world/events/EventResult.h>
+#include <mc/world/events/PlayerOpenContainerEvent.h>
 
 namespace ila::mc::inline world::inline actor::inline player {
 
 void PlayerOpenContainerBeforeEvent::serialize(CompoundTag& nbt) const {
     Cancellable::serialize(nbt);
     nbt["containerBlockPos"] = ListTag{containerBlockPos().x, containerBlockPos().y, containerBlockPos().z};
-    nbt["containerId"]       = static_cast<schar>(containerId());
     nbt["containerType"]     = magic_enum::enum_name(containerType());
     nbt["containerActorId"]  = containerActorId().rawID;
 }
@@ -18,71 +21,61 @@ void PlayerOpenContainerBeforeEvent::deserialize(CompoundTag const& nbt) {
     containerBlockPos().x = nbt["containerBlockPos"][0];
     containerBlockPos().y = nbt["containerBlockPos"][1];
     containerBlockPos().z = nbt["containerBlockPos"][2];
-    containerId()         = static_cast<ContainerID>(nbt["containerId"].get<ByteTag>().data);
     containerType() = magic_enum::enum_cast<SharedTypes::Legacy::ContainerType>(nbt["containerType"].get<StringTag>())
                           .value_or(containerType());
     containerActorId().rawID = nbt["containerActorId"];
 }
 BlockPos&                           PlayerOpenContainerBeforeEvent::containerBlockPos() const { return mPos; }
-ContainerID&                        PlayerOpenContainerBeforeEvent::containerId() const { return mContainerId; }
 SharedTypes::Legacy::ContainerType& PlayerOpenContainerBeforeEvent::containerType() const { return mContainerType; }
 ActorUniqueID& PlayerOpenContainerBeforeEvent::containerActorId() const { return mContainerActorId; }
 
 void PlayerOpenContainerAfterEvent::serialize(CompoundTag& nbt) const {
-    ServerPlayerEvent::serialize(nbt);
+    PlayerEvent::serialize(nbt);
     nbt["containerBlockPos"] = ListTag{containerBlockPos().x, containerBlockPos().y, containerBlockPos().z};
-    nbt["containerId"]       = static_cast<schar>(containerId());
     nbt["containerType"]     = magic_enum::enum_name(containerType());
     nbt["containerActorId"]  = containerActorId().rawID;
 }
 BlockPos const&                           PlayerOpenContainerAfterEvent::containerBlockPos() const { return mPos; }
-ContainerID const&                        PlayerOpenContainerAfterEvent::containerId() const { return mContainerId; }
 SharedTypes::Legacy::ContainerType const& PlayerOpenContainerAfterEvent::containerType() const {
     return mContainerType;
 }
 ActorUniqueID const& PlayerOpenContainerAfterEvent::containerActorId() const { return mContainerActorId; }
 
-Event_Listener_Factory(PlayerOpenContainerBefore) {
-    nextTick([this]() -> void { // Prevent deadlock
-        mListeners.emplace_back(LLEventBus.emplaceListener<ila::mc::server::SendPacketBeforeEvent<ContainerOpenPacket>>(
-            [](ila::mc::server::SendPacketBeforeEvent<ContainerOpenPacket>& event) -> void {
-                if (auto player = event.player(); player) {
-                    auto& packet      = event.packet();
-                    auto  beforeEvent = PlayerOpenContainerBeforeEvent(
-                        *event.player(),
-                        *packet.mPos,
-                        packet.mContainerId,
-                        packet.mType,
-                        *packet.mEntityUniqueID
-                    );
-                    LLEventBus.publish(beforeEvent);
-                    if (beforeEvent.isCancelled()) {
-                        event.cancel();
-                        event.player()->doDeleteContainerManager(false);
-                    }
-                }
-            }
-        ));
-    });
+LL_TYPE_INSTANCE_HOOK(
+    OpenContainerHook,
+    HookPriority::Normal,
+    VanillaServerGameplayEventListener,
+    &VanillaServerGameplayEventListener::$onEvent,
+    EventResult,
+    PlayerOpenContainerEvent const& playerOpenContainerEvent
+) {
+    Actor* actor = playerOpenContainerEvent.mPlayer->tryUnwrap<Actor>();
+    if (actor && actor->isType(ActorType::Player)) {
+        Player& player      = *static_cast<Player*>(actor);
+        auto    beforeEvent = PlayerOpenContainerBeforeEvent(
+            player,
+            const_cast<BlockPos&>(*playerOpenContainerEvent.mBlockPos),
+            const_cast<SharedTypes::Legacy::ContainerType&>(playerOpenContainerEvent.mContainerType),
+            const_cast<ActorUniqueID&>(*playerOpenContainerEvent.mEntityUniqueId)
+        );
+        LLEventBus.publish(beforeEvent);
+        if (beforeEvent.isCancelled()) {
+            return EventResult::StopProcessing;
+        }
+        auto result = origin(playerOpenContainerEvent);
+        if (result == EventResult::KeepGoing) {
+            LLEventBus.publish(PlayerOpenContainerAfterEvent(
+                player,
+                playerOpenContainerEvent.mBlockPos,
+                playerOpenContainerEvent.mContainerType,
+                playerOpenContainerEvent.mEntityUniqueId
+            ));
+        }
+        return result;
+    }
+    return origin(playerOpenContainerEvent);
 }
 
-Event_Listener_Factory(PlayerOpenContainerAfter) {
-    nextTick([this]() -> void { // Prevent deadlock
-        mListeners.emplace_back(LLEventBus.emplaceListener<ila::mc::server::SendPacketAfterEvent<ContainerOpenPacket>>(
-            [](ila::mc::server::SendPacketAfterEvent<ContainerOpenPacket>& event) -> void {
-                if (auto player = event.player(); player) {
-                    auto& packet = event.packet();
-                    LLEventBus.publish(PlayerOpenContainerAfterEvent(
-                        *event.player(),
-                        *packet.mPos,
-                        packet.mContainerId,
-                        packet.mType,
-                        *packet.mEntityUniqueID
-                    ));
-                }
-            }
-        ));
-    });
-}
+Event_Hook_Factory(PlayerOpenContainer, <OpenContainerHook>);
 
 } // namespace ila::mc::inline world::inline actor::inline player
--- src/ila/event/world/actor/player/PlayerOpenContainerEvent.h
+++ src/ila/event/world/actor/player/PlayerOpenContainerEvent.h
@@ -1,6 +1,6 @@
 #include "ila/base/Macro.h"
 #include <ll/api/event/Cancellable.h>
-#include <ll/api/event/player/ServerPlayerEvent.h>
+#include <ll/api/event/player/PlayerEvent.h>
 
 // clang-format off
 class BlockPos;
@@ -8,61 +8,53 @@ struct ActorUniqueID;
 // clang-format on
 
 namespace ila::mc::inline world::inline actor::inline player {
-class PlayerOpenContainerBeforeEvent final : public ll::event::Cancellable<ll::event::player::ServerPlayerEvent> {
+class PlayerOpenContainerBeforeEvent final : public ll::event::Cancellable<ll::event::player::PlayerEvent> {
 protected:
     BlockPos&                           mPos;
-    ContainerID&                        mContainerId;
     SharedTypes::Legacy::ContainerType& mContainerType;
     ActorUniqueID&                      mContainerActorId;
 
 public:
     constexpr explicit PlayerOpenContainerBeforeEvent(
-        ServerPlayer&                       player,
+        Player&                             player,
         BlockPos&                           pos,
-        ContainerID&                        containerId,
         SharedTypes::Legacy::ContainerType& containerType,
         ActorUniqueID&                      containerActorId
     )
     : Cancellable(player),
       mPos(pos),
-      mContainerId(containerId),
       mContainerType(containerType),
       mContainerActorId(containerActorId) {}
 
     ILAPI void serialize(CompoundTag& nbt) const override;
     ILAPI void deserialize(CompoundTag const& nbt) override;
 
-    ILNDAPI BlockPos&    containerBlockPos() const;
-    ILNDAPI ContainerID& containerId() const;
+    ILNDAPI BlockPos& containerBlockPos() const;
     ILNDAPI SharedTypes::Legacy::ContainerType& containerType() const;
     ILNDAPI ActorUniqueID&                      containerActorId() const;
 };
 
-class PlayerOpenContainerAfterEvent final : public ll::event::player::ServerPlayerEvent {
+class PlayerOpenContainerAfterEvent final : public ll::event::player::PlayerEvent {
 protected:
     BlockPos const&                           mPos;
-    ContainerID const&                        mContainerId;
     SharedTypes::Legacy::ContainerType const& mContainerType;
     ActorUniqueID const&                      mContainerActorId;
 
 public:
     constexpr explicit PlayerOpenContainerAfterEvent(
-        ServerPlayer&                             player,
+        Player&                                   player,
         BlockPos const&                           pos,
-        ContainerID const&                        containerId,
         SharedTypes::Legacy::ContainerType const& containerType,
         ActorUniqueID const&                      containerActorId
     )
-    : ServerPlayerEvent(player),
+    : PlayerEvent(player),
       mPos(pos),
-      mContainerId(containerId),
       mContainerType(containerType),
       mContainerActorId(containerActorId) {}
 
     ILAPI void serialize(CompoundTag& nbt) const override;
 
-    ILNDAPI BlockPos const&    containerBlockPos() const;
-    ILNDAPI ContainerID const& containerId() const;
+    ILNDAPI BlockPos const& containerBlockPos() const;
     ILNDAPI SharedTypes::Legacy::ContainerType const& containerType() const;
     ILNDAPI ActorUniqueID const&                      containerActorId() const;
 };
